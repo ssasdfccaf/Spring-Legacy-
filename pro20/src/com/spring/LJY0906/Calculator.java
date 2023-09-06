672fed7fb2ba5dbb7507d7d4e5d4d22d195f08cb
이재영 <139414509+jaeyeong4501@users.noreply.github.com>
2023-09-06 17:11:52 +0900

    230907이재영 과제제출

---
+++ pro20/src/com/spring/LJY0906/Calculator.java
@@ -0,0 +1,23 @@
+package com.spring.LJY0906;
+
+public class Calculator {
+	public void add(int x, int y) {
+		int result=x+y;
+		System.out.println("���:"+ result);
+	}
+
+	public void subtract(int x, int y) {
+		int result=x - y;
+		System.out.println("���:"+ result);
+	}
+
+	public void multiply(int x, int y) {
+		int result=x * y;
+		System.out.println("���:"+ result);
+	}
+
+	public void divide(int x, int y) {
+		int result=x / y;
+		System.out.println("���:"+ result);
+	}
+}
