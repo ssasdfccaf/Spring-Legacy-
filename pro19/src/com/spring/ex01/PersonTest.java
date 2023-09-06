1a894874de998d198d94df08bd2f79a3686d842a
gilin03 <tb081212@naver.com>
2023-09-06 12:12:52 +0900

    수정

--- pro19/src/com/spring/ex01/PersonTest.java
+++ pro19/src/com/spring/ex01/PersonTest.java
@@ -26,7 +26,6 @@ public class PersonTest {
 		// 메인 메소드가 있는 클래스에서 -> 우클릭에서, 자바 로 단순 실행. 
 		//
 		person.sayHello();
-		//homework브랜치입니다.
 		
 		//평상시, PersonServiceImpl
 		System.out.println("보통 일반적인 개발자들이 객체 인스턴스를 생성하는 경우.");
