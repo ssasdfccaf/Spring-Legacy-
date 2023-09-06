42cddc3b9ea32204c8f499a48d5e692039f1c5ef
lsy3709 <lsy3709@naver.com>
2023-09-06 16:33:58 +0900

    230906 7교시 과제 안내 저장

---
+++ pro20/src/com/spring/lsy0906/LoggingAdvice.java
@@ -0,0 +1,20 @@
+package com.spring.lsy0906;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+// 보조기능 클래스 , MethodInterceptor 인터페이스 구현 후, 추상 메서드 의무적 재정의
+public class LoggingAdvice implements MethodInterceptor {
+	// 주기능이 1) 실행 되기 전, 2) 실행 되기 후, 3) 예외발생시 
+	// 이 invoke 메서드가 실행이됨. 그렇게 설계됨. 
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		System.out.println("[메서드 호출 전 : LogginAdvice");
+		System.out.println(invocation.getMethod() + "메서드 호출 전");
+
+		Object object = invocation.proceed();
+
+		System.out.println("[메서드 호출 후 : loggingAdvice");
+		System.out.println(invocation.getMethod() + "메서드 호출 후");
+		return object;
+	}
+}
