16e92737fb23a07c8f612aac7341a8bb976af8a2
김다영 <sksgurrnek01@naver.com>
2023-09-06 12:19:22 +0900

    KdyTest2.java 두번째 테스트

---
+++ pro19/src/com/spring/kdy0906/KdyTest2.java
@@ -0,0 +1,10 @@
+package com.spring.kdy0906;
+
+public class KdyTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("두번째 깃 테스트 중 김다영");
+	}
+
+}
