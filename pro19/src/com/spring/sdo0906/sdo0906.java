874db41999daa602f18baa9e0af6b0696c32a6f6
eastok <eastok95@gmail.com>
2023-09-06 12:31:11 +0900

    숙제 수정 4교시

--- pro19/src/com/spring/sdo0906/sdo0906.java
+++ pro19/src/com/spring/sdo0906/sdo0906.java
@@ -3,7 +3,7 @@ package com.spring.sdo0906;
 public class sdo0906 {
 	// 메인 메서드에서, 
 		public static void main(String[] args) {
-			System.out.println("서 동옥");
+			System.out.println("서 동옥 수정본");
 			
 		}
 
