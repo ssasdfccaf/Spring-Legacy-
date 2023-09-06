a0f4f599bc2d94fde21af7d228187f1b2bd9d9f1
gilin03 <tb081212@naver.com>
2023-09-06 10:43:02 +0900

    ktb

--- pro19/src/com/spring/ktb0906/ktbTest.java
+++ pro19/src/com/spring/ktb0906/ktbTest.java
@@ -7,5 +7,7 @@ public class ktbTest {
 	// 메인 메서드에서, 
 	public static void main(String[] args) {
 		System.out.println("깃 테스트");
+		System.out.println("깃 테스트");
+		System.out.println("깃 테스트");
 	}
 }
