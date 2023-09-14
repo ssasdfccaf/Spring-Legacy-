b2124270258dc98c32e1a92935c60e0eacff8c85
lsy3709 <lsy3709@naver.com>
2023-09-14 11:15:42 +0900

    230914 3교시 동네2번 서비스 부분 변경

--- pro26/src/com/spring/member/service/MemberServiceImpl.java
+++ pro26/src/com/spring/member/service/MemberServiceImpl.java
@@ -24,6 +24,20 @@ public class MemberServiceImpl implements MemberService {
 		return membersList;
 	}
 
+	  // 한 회원의 정보 가져오기 추가 부분. 
+	   @Override
+		public MemberVO getOneMember(String id) throws DataAccessException {
+		   MemberVO membervo = null;
+		   // 실제 작업, 동네 3번, dao 외주 주기. 
+		   membervo = memberDAO.selectOneMember(id);
+			return membervo;
+		}
+//추가, 수정 반영하기 
+	   @Override
+		public int updateMember(MemberVO memberVO) throws DataAccessException {
+		   return memberDAO.updateMember(memberVO);
+		}
+	
 	@Override
 	public int addMember(MemberVO member) throws DataAccessException {
 		return memberDAO.insertMember(member);
@@ -33,4 +47,6 @@ public class MemberServiceImpl implements MemberService {
 	public int removeMember(String id) throws DataAccessException {
 		return memberDAO.deleteMember(id);
 	}
+	
+	
 }
