fd25c5fc4ff2288ba5d09ddb09c980a2a1301ba7
lsy3709 <lsy3709@naver.com>
2023-09-21 12:04:47 +0900

    230921 4교시 시작

---
+++ bookShop01/src/main/java/com/bookshop01/main/MainController.java
@@ -0,0 +1,42 @@
+package com.bookshop01.main;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bookshop01.common.base.BaseController;
+import com.bookshop01.goods.service.GoodsService;
+import com.bookshop01.goods.vo.GoodsVO;
+
+@Controller("mainController")
+@EnableAspectJAutoProxy
+public class MainController extends BaseController {
+	@Autowired
+	private GoodsService goodsService;
+
+	@RequestMapping(value= {"/","/main.do","/main/main.do"} ,method={RequestMethod.POST,RequestMethod.GET})
+	public ModelAndView main(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		HttpSession session;
+		ModelAndView mav=new ModelAndView();
+		
+		String viewName=(String)request.getAttribute("viewName");
+		System.out.println("viewName 컨트롤러 도달 전 뷰이름 가져오기 : " +viewName );
+		mav.setViewName(viewName);
+		
+		session=request.getSession();
+		session.setAttribute("side_menu", "user");
+		Map<String,List<GoodsVO>> goodsMap=goodsService.listGoods();
+		mav.addObject("goodsMap", goodsMap);
+		return mav;
+	}
+}
